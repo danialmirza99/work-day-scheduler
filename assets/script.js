1df5fdc34f871c1f6c6b6dc2c4d0d45b5276a2cc
danialmirza99 <danialmirza99@gmail.com>
2022-10-06 22:29:03 -0700

    add tasks to schedule

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,12 @@
 var currentDate = moment().format('dddd MMMM Do YYYY');
 $("#currentDay").text("Current Date: " + currentDate);
 
+// color calendar based on past, present, future
+var time = moment().format("h a");
+var tasks = document.querySelector(".list-group-item");
+for(var i = 0; i < tasks.clientHeight; i++){
+    console.log(tasks);
+}
+
+
+
